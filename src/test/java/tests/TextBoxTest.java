fb4f816276dba3fb142d5a8592eaa379e1a9221d
Tsareva Darya <d.tsareva@space307.com>
2024-07-10 20:47:54 +0200

    added homework task

--- src/test/java/tests/TextBoxTest.java
+++ src/test/java/tests/TextBoxTest.java
@@ -4,11 +4,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
 
-    public class TextBoxTest {
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTest {
 
         @BeforeAll
         static void beforeAll() {
@@ -21,17 +23,43 @@ import static com.codeborne.selenide.Selenide.open;
 
         @Test
         void fillFormTest() {
-            open("/text-box");
-            $("#userName").setValue("Dasha");
+            open("/automation-practice-form");
+            $("#firstName").setValue("Dasha");
+            $("#lastName").setValue("Tsareva");
             $("#userEmail").setValue("dasha55@mail.ru");
-            $("#currentAddress").setValue("Street 45");
-            $("#permanentAddress").setValue("Street 47");
-            $("#submit").click();
+            $(byText("Female")).parent().click();
+            $("#userNumber").setValue("89991234567");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__year-select").selectOption("2022");
+            $(".react-datepicker__month-select").selectOption("July");
+            $(".react-datepicker__day--009").click();
+            $(".subjectsInput").setValue("M");
+            $x("//div[contains(text(),'Maths')]").click();
+            $(byText("Sports")).parent().click();
+            $(byText("Music")).parent().click();
+            $("#uploadPicture").uploadFile(new File("student.jpg"));
+            $("#currentAddress").setValue("City");
+            $x("//div[@id='state']").click();
+            $x("//div[contains(text(),'NCR')]").click();
+            $x("//div[@id='city']").click();
+            $x("//div[contains(text(),'Delhi')]").click();
 
-            $("#output #name").shouldHave(text("Dasha"));
-            $("#output #email").shouldHave(text("dasha55@mail.ru"));
-            $("#output #currentAddress").shouldHave(text("Street 45"));
-            $("#output #permanentAddress").shouldHave(text("Street 47"));
+
+
+            // проверки
+            $(".modal-body").shouldHave(text("Dasha Tsareva"));
+            $(".modal-body").shouldHave(text("dasha55@mail.ru"));
+            $(".modal-body").shouldHave(text("Female"));
+            $(".modal-body").shouldHave(text("89991234567"));
+            $(".modal-body").shouldHave(text("9 July,2022"));
+           // $(".modal-body").shouldHave(text("Maths"));
+            $(".modal-body").shouldHave(text("Sports, Music"));
+            $("#uploadPicture").parent().shouldHave(text("student.jpg"));
+            $(".modal-body").shouldHave(text("Dasha Tsareva"));
+
+
+
+
         }
     }
 
